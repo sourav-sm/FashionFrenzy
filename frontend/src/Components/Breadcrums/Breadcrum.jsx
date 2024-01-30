2a11fc6dcb9d82bb495728a1061cccfed9d7967d
Sourav <souravmohanta4428@gmail.com>
2024-01-31 02:30:48 +0530

    created product dosplay part

---
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './Breadcrum.css'
+import arrow_icon from '../Assets/Breadcrum_arrow.png'
+
+const Breadcrum= (props)=>{
+     const {product}=props;
+    return (
+    <div>
+        <div className='breadcrum'>
+            Home <img src={arrow_icon} alt="" />
+            Shop <img src={arrow_icon} alt="" />
+            {product.category} <img src={arrow_icon} alt="" />
+            {product.name}
+        </div>
+    </div>
+ )
+}
+export default Breadcrum;
--- frontend/src/Pages/LoggingSignup.jsx
+++ frontend/src/Pages/LoggingSignup.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import './CSS/LoggingSignup.css';
 
 const LoggingSignup=()=>{
     return(
-        <div>
-
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email Adress" />
+                    <input type="password" placeholder="Password" />    
+                </div>
+                <button>Continue</button>
+                <p className="loginsignup-login">Already have an Account ? <span>Login</span></p>
+                <div className="loginsignup-agree">
+                    <input type="checkbox" name='' id=''/>
+                    <p>By continuing,i agree to the term of use and privacy policy</p>
+                </div>
+            </div>
         </div>
     )
 }
