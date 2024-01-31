bcc5d0f8be38a26b9050975ab210f6fb13ddad16
Sourav <souravmohanta4428@gmail.com>
2024-01-31 10:44:28 +0530

    complete productdisplaypage

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -20,7 +20,7 @@ const ProductDisplay=(props)=>{
             </div>
             <div className="productdisplay-right">
                 <h1>{product.name}</h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
@@ -28,8 +28,8 @@ const ProductDisplay=(props)=>{
                     <p>(122)</p>
                 </div>
             <div className="productdisplay-right-prices">
-                <div className="productdisplay-right-price-old">{product.old_price}</div>
-                <div className="productdisplay-right-price-new">{product.new_price}</div>
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
             </div>
             <div className="productdisplay-right-description">
                 A light nacxncn anczkjnnkncncncncadncdjjjjjjjjjjjjjjjjjjjjjjjjjjj
@@ -39,7 +39,7 @@ const ProductDisplay=(props)=>{
             </div>
             <div className="productdisplay-right-size">
                 <h1>Select Size</h1>
-                <div className="productdisplay-right-size">
+                <div className="productdisplay-right-sizes">
                     <div>S</div>
                     <div>M</div>
                     <div>L</div>
