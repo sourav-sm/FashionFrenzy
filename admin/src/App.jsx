12e140aa58cba60c7273b0d038dab343f0b1a6ba
Sourav <souravmohanta4428@gmail.com>
2024-02-04 04:15:42 +0530

    working on  admin panel amke it responsiveren and adding feature of imagehandler

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from "./Components/Navbar/Navbar"
+import Admin from './Pages/Admin/Admin'
 
 function App() {
   return (
       <div>
        <Navbar/>
+       <Admin/>
       </div>
   )
 }
---
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import './AddProduct.css';
+import upload_area from '../../assets/upload_area.svg'
+
+const AddProduct=()=>{
+    const [image,setImage]= useState(false);
+
+    const imageHandler = (e)=>{
+        setImage(e.target.files[0]);
+    }
+
+    return(
+        <div className="add-product">
+           <div className="addproduct-itemfiled">
+              <p>Product Title</p>
+              <input type="text" name='name' placeholder="Type Here" />
+            </div> 
+            <div className="addproduct-price">
+                <div className="addproduct-itemfield">
+                    <p>Price</p>
+                    <input type="text" name="old_price" placeholder="Type Here"/>
+                </div>
+                <div className="addproduct-itemfield">
+                    <p>Offer Price</p>
+                    <input type="text" name="new_price" placeholder="Type Here"/>
+                </div>
+            </div>
+            <div className="addproduct-itemfield">
+                <p>Product Category</p>
+                <select name="category" className="add-product-selector">
+                    <option value="women">Women</option>
+                    <option value="men">Men</option>
+                    <option value="kid">Kid</option>
+                </select>
+            </div>
+            <div className="addproduct-itemfield">
+                <label htmlFor="file-input">
+                    <img src={image?URL.createObjectURL(image):upload_area} className="addproduct-thumnail-img" />
+                </label>
+                <input onChange={imageHandler} type="file" name='image' id="file-input" hidden/>
+            </div>
+            <button className="addproduct-btn">ADD</button>
+        </div>
+    )
+}
+export default AddProduct;
