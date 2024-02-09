2bf2e8fd4df77bf57e03e56ab7b32204d8a33a44
Sourav <souravmohanta4428@gmail.com>
2024-02-09 20:37:37 +0530

    fix bug of breadcrum_arrow.png

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './Breadcrum.css'
-import arrow_icon from '../Assets/Breadcrum_arrow.png'
+// import arrow_icon from '../Assets/Breadcrum_arrow.png'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadcrum= (props)=>{
      const {product}=props;
