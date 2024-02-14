504ede7a7e3bd27a0474d13bf61025aa6699c226
Sourav <souravmohanta4428@gmail.com>
2024-02-14 18:00:02 +0530

    updating ui

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -40,27 +40,27 @@
 
 import React from "react";
 import './Hero.css';
-import hand_icon from '../Assets/logo1.jpg';
+//import hand_icon from '../Assets/logo1.jpg';
 import arrow_icon from '../Assets/arrow.png';
-import hero_image from '../Assets/hero2.png';
+import hero_image from '../Assets/model_big1.jpg';
 import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>Discover Our Latest Arrivals</h2>
+        <h2>Trendy Collections</h2>
         <div>
           <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt="" />
+            <p>Shop For What You Want</p>
           </div>
-          <i> Exclusive Collections </i>
-          <p>for Every Shopper!</p>
         </div>
+        <div className="hero-hand-icon-pgp">
+          Unwrap convenience, shop with ease, and indulge in endless possibilities with us!
+          </div>
         <Link to="/new-collections" className="hero-latest-btn">
           <div>Latest Collection</div>
-          <img src={arrow_icon} alt="" />
+          {/* <img src={arrow_icon} alt="" /> */}
         </Link>
       </div>
       <div className="hero-right">
