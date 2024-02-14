1f4e4d6b5c1b966bdaa48848ab2eab1b8cd6462d
Sourav <souravmohanta4428@gmail.com>
2024-02-14 18:46:10 +0530

    updating ui

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,43 +1,3 @@
-// import React from "react"
-// import './Hero.css'
-// import hand_icon from '../Assets/hand_icon.png'
-// import arrow_icon from '../Assets/arrow.png'
-// import hero_image from '../Assets/hero2.png'
-// import { Link } from "react-router-dom"
-// import NewCollections from "../NewCollections/NewCollection"
-
-// const Hero=()=>{
-//   return (
-//     <div className="hero">
-//         <div className="hero-left">
-//             <h2>NEW ARRIVALS ONLY</h2>
-//             <div>
-//                 <div className="hero-hand-icon">
-//                     <p>new</p>
-//                     <img src={hand_icon} alt="" />
-//                 </div>
-//                 <i>COLLECTIONS</i>
-//                 <p>for everyone</p>
-//             </div>
-//             { <div className="hero-latest-btn" onClick={()=><NewCollections/>}>
-//                 {/* <div>Latest Collection</div>
-//                 <img src={arrow_icon} alt="" /> */}
-//                  <Link to="/new-collections" className="hero-latest-btn">
-//           <div>Latest Collection</div>
-//           <img src={arrow_icon} alt="" />
-//           </Link>
-//          </div> }
-             
-//         </div>
-//         <div className="hero-right">
-//             <img  src={hero_image} alt="" />
-//         </div>  
-//   </div>
-//   )
-// }
-
-// export default Hero;
-
 import React from "react";
 import './Hero.css';
 //import hand_icon from '../Assets/logo1.jpg';
@@ -56,11 +16,11 @@ const Hero = () => {
           </div>
         </div>
         <div className="hero-hand-icon-pgp">
-          Unwrap convenience, shop with ease, and indulge in endless possibilities with us!
+          Unwrap convenience, shop with ease, and indulge <br/>in endless possibilities with us!
           </div>
         <Link to="/new-collections" className="hero-latest-btn">
           <div>Latest Collection</div>
-          {/* <img src={arrow_icon} alt="" /> */}
+          <img src={arrow_icon} alt="" />
         </Link>
       </div>
       <div className="hero-right">
